e2be3241dbb2b6e0357a77b27c3ff100d8eb514e
Jhonny <jn.tech.377@gmail.com>
2026-02-22 14:02:02 -0300

    first commit

---
+++ src/main/java/com/jhonathan/loja3d/Loja3dApplication.java
@@ -0,0 +1,13 @@
+package com.jhonathan.loja3d;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Loja3dApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Loja3dApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jhonathan/loja3d/Loja3dApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jhonathan.loja3d;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Loja3dApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
